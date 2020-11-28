957e50fc0de54417d5cf061a444299812a3671e2
maxym Myroshnychenko <mmyros@gmail.com>
2020-11-28 13:39:22 -0500

    Tests

---
+++ tests/test_estimate.py
@@ -0,0 +1,14 @@
+from estimate import main
+
+
+def test_main():
+    # TODO emulate argv; this is too difficult for me. Consider click or argparse? Those are easy to test
+    # argv = 'sample_data/spike_times.dat \
+    # --settings-file settings/test.yaml \
+    # --output sample_analysis.pdf'
+    # main()
+    # utl.get_spike_times_from_file
+    # main('sample_data/spike_times.dat \
+    # --settings-file settings/test.yaml \
+    # --output sample_analysis.pdf')
+    assert True
